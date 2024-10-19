0cf516bcd6d7f7d1ecafb4dbba084e2822aee2df
Hervé Pagès <hpages.on.github@gmail.com>
2024-10-19 16:05:11 -0700

    will keep it deprecated in BioC 3.21, and remove from BioC 3.22

--- R/zzz.R
+++ R/zzz.R
@@ -48,6 +48,6 @@ pkgconfig <-
 .onAttach <- function(libname, pkgname) {
     msg <- sprintf(
         "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.21")
+         version %s", pkgname, "3.22")
     .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
